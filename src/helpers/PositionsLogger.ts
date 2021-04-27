003ac9f579615c3f6e8cd62088f1df09d129d4f5
BSalim <burhan.salman@gmail.com>
2021-04-28 03:34:27 +0400

    Rearrange colums and add gain loss dollar to avg

--- src/helpers/PositionsLogger.ts
+++ src/helpers/PositionsLogger.ts
@@ -14,12 +14,12 @@ export class PositionsLogger {
         { name: 'date', title: 'Date' },
         { name: 'ticker', title: 'Ticker' },
         { name: 'amount', title: 'Amount' },
+        { name: 'gainLoss', title: 'Gain/Loss $' },
+        { name: 'gainLossPercentage', title: 'Gain/Loss %' },
         { name: 'openingPrice', title: 'Opening Price' },
         { name: 'currentPrice', title: 'Current Price' },
         { name: 'openingCost', title: 'Opening Cost' },
         { name: 'currentCost', title: 'Current Cost' },
-        { name: 'gainLoss', title: 'Gain/Loss' },
-        { name: 'gainLossPercentage', title: 'Gain/Loss %' },
       ],
       sort: (a, b) => (a.ticker < b.ticker ? -1 : a.ticker > b.ticker ? 1 : 0),
     });
@@ -34,7 +34,7 @@ export class PositionsLogger {
         openingCost: p.openingCost.toPrecision(5),
         currentCost: p.currentCost.toPrecision(5),
         gainLoss: p.gainLoss.toFixed(2),
-        gainLossPercentage: p.gainLossPercentage.toPrecision(5),
+        gainLossPercentage: p.gainLossPercentage.toFixed(2),
       };
       table.addRow(display, { color: p.gainLossPercentage >= 0 ? 'green' : 'red' });
     });
@@ -45,23 +45,24 @@ export class PositionsLogger {
   logCumulative() {
     const openingTotalCost = this.openPositions.reduce((acc, pos) => acc + pos.openingCost, 0);
     const currentTotalCost = this.openPositions.reduce((acc, pos) => acc + pos.currentCost, 0);
-    const gainLoss = (currentTotalCost / openingTotalCost - 1) * 100;
+    const gainLossPercent = (currentTotalCost / openingTotalCost - 1) * 100;
 
     const table = new Table();
     table.addRow(
       {
         'Opening Cost Total': openingTotalCost.toPrecision(5),
         'Current Cost Total': currentTotalCost.toPrecision(5),
-        'Gain/Loss': gainLoss.toPrecision(3),
+        'Gain/Loss $': (currentTotalCost - openingTotalCost).toFixed(2),
+        'Gain/Loss %': gainLossPercent.toPrecision(3),
       },
-      { color: gainLoss >= 0 ? 'green' : 'red' }
+      { color: gainLossPercent >= 0 ? 'green' : 'red' }
     );
 
     table.printTable();
   }
 
   logAvgCostPerCurreny() {
-    const totalCostByTicker = this.openPositions.reduce((acc, pos) => {
+    const totalOpeningCostByTicker = this.openPositions.reduce((acc, pos) => {
       acc[pos.ticker] = (acc[pos.ticker] ?? 0) + pos.openingCost;
       return acc;
     }, {});
@@ -70,28 +71,32 @@ export class PositionsLogger {
       return acc;
     }, {});
 
-    const rows = Object.keys(totalCostByTicker).map((ticker) => {
-      const avgPrice = totalCostByTicker[ticker] / totalAmountByTicker[ticker];
-      const currentPrice =
-        this.openPositions.find((pos) => pos.ticker === ticker).currentPrice;
+    const rows = Object.keys(totalOpeningCostByTicker).map((ticker) => {
+      const avgPrice = totalOpeningCostByTicker[ticker] / totalAmountByTicker[ticker];
+      const currentPrice = this.openPositions.find((pos) => pos.ticker === ticker).currentPrice;
+      const currentCost = currentPrice * totalAmountByTicker[ticker];
       return {
         ticker: ticker,
+        amount: totalAmountByTicker[ticker].toPrecision(5),
         avgCost: (avgPrice * totalAmountByTicker[ticker]).toPrecision(5),
-        currentCost: (currentPrice * totalAmountByTicker[ticker]).toPrecision(5),
+        currentCost: (currentCost).toPrecision(5),
         avgPrice: avgPrice.toPrecision(5),
         currentPrice: currentPrice.toPrecision(5),
-        gainLoss: ((currentPrice / avgPrice - 1) * 100).toPrecision(3),
+        gainLossPercent: ((currentPrice / avgPrice - 1) * 100).toPrecision(3),
+        gainLoss: (currentCost - totalOpeningCostByTicker[ticker]).toFixed(2),
       };
     });
 
     const table = new Table({
       columns: [
         { name: 'ticker', title: 'Ticker' },
+        { name: 'amount', title: 'Amount' },
+        { name: 'gainLoss', title: 'Gain/Loss $' },
+        { name: 'gainLossPercent', title: 'Gain/Loss %' },
         { name: 'avgCost', title: 'Avg. Cost' },
         { name: 'currentCost', title: 'Current Cost' },
         { name: 'avgPrice', title: 'Avg. Price' },
         { name: 'currentPrice', title: 'Current Price' },
-        { name: 'gainLoss', title: 'Gain/Loss' },
       ],
     });
     rows.forEach((row) => {
