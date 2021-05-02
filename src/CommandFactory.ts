257bc06ebdae4fbf7a46367ed70dfe9b75575403
BSalim <burhan.salman@gmail.com>
2021-05-02 07:08:49 +0400

    Throw error for command not found

--- src/CommandFactory.ts
+++ src/CommandFactory.ts
@@ -4,13 +4,16 @@ import { OpenCommand } from './commands/OpenCommand';
 
 export class CommandFactory {
   private commandMapping: ICommand[] = [new OpenCommand()];
-  private defaultCommand: ICommand = undefined;
   constructor() {}
 
   create(data: ICommandData): ICommand {
-    const command = this.commandMapping.find((cmd) => cmd.name === data.name) || this.defaultCommand;
-    command.setArguments(data.args);
+    const command = this.commandMapping.find((cmd) => cmd.name === data.name);
 
+    if (!command) {
+      throw new Error(`Command does not exist: ${data.name}`);
+    }
+
+    command.setArguments(data.args);
     return command;
   }
 }
