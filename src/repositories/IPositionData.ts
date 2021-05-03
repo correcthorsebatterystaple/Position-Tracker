f8e6aff3ea9f780bbb1fb2eeb3d3e59845333064
BSalim <burhan.salman@gmail.com>
2021-05-03 07:01:44 +0400

    Add enum for position status

--- src/repositories/IPositionData.ts
+++ src/repositories/IPositionData.ts
@@ -1,9 +1,11 @@
+import { PositionStatus } from "../enums/PositionStatusEnum";
+
 export interface IPositionData {
   id: string;
   date: number;
   ticker: string;
   amount: number;
   opening_price: number;
-  status: 'OPEN' | 'CLOSED';
+  status: PositionStatus;
   closing_price?: number;
 }
