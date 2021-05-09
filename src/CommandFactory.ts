7b65020798d60352aafd0415ae4f8238a053f430
BSalim <burhan.salman@gmail.com>
2021-05-09 06:38:59 +0400

    Add buy command

--- src/CommandFactory.ts
+++ src/CommandFactory.ts
@@ -4,9 +4,16 @@ import { ICommandData } from './interfaces/ICommandData';
 import { LogCommand } from './commands/LogCommand';
 import { OpenCommand } from './commands/OpenCommand';
 import { PriceCommand } from './commands/PriceCommand';
+import { BuyCommand } from './commands/BuyCommand';
 
 export class CommandFactory {
-  private commandMapping: ICommand[] = [new OpenCommand(), new CloseCommand(), new PriceCommand(), new LogCommand()];
+  private commandMapping: ICommand[] = [
+    new OpenCommand(),
+    new CloseCommand(),
+    new PriceCommand(),
+    new LogCommand(),
+    new BuyCommand(),
+  ];
   constructor() {
     // do nothing
   }
---
+++ src/commands/BuyCommand.ts
@@ -0,0 +1,44 @@
+import minimist from 'minimist';
+import { Logger } from '../helpers/Logger';
+import { ICommand } from '../interfaces/ICommand';
+import { Trade } from '../models/Trade';
+import { TradeRepository } from '../repositories/TradeRepository';
+
+export class BuyCommand implements ICommand {
+  name = 'buy';
+  private args: { symbol: string; cost: number; amount: number; date: string};
+  private readonly OTHER_SYMBOL = 'USDT';
+  private tradeRepository = new TradeRepository(); 
+
+  constructor() {
+    // do nothing
+  }
+
+  setArguments(args: string[]): void {
+    const parsedArgs = minimist(args, { string: ['symbol'] });
+    this.args = {
+      date: parsedArgs.date,
+      symbol: parsedArgs.symbol,
+      cost: parsedArgs.cost,
+      amount: parsedArgs.amount,
+    };
+
+    if (!(this.args.amount && this.args.cost && this.args.symbol)) {
+      throw new Error('--symbol, --price and --amount are required parameters');
+    }
+  }
+
+  async execute(): Promise<void> {
+    const trade: Omit<Trade, 'id'> = {
+      date: new Date(this.args.date ?? Date.now()).valueOf(),
+      buySymbol: this.args.symbol.toUpperCase(),
+      buyAmount: this.args.amount,
+      sellAmount: this.args.cost,
+      sellSymbol: this.OTHER_SYMBOL,
+    };
+
+    await this.tradeRepository.addTrade(trade);
+
+    Logger.OK(`Bought ${this.args.symbol.toUpperCase()} for $${this.args.cost}`);
+  }
+}
